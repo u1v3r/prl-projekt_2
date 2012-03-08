670e2e54c0ce255398ce602aeb06e0e35819fe7a
Radovan Dvorsky <rdvorsky6@gmail.com>
2012-03-08 22:46:32 +0100

    prvy pokus

---
+++ pms.h
@@ -0,0 +1,16 @@
+#ifndef PMS_H_INCLUDED
+#define PMS_H_INCLUDED
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <getopt.h>
+
+#define FILENAME "numbers"
+#define TAG 0
+#define INIT_NUMBERS_SIZE 50    /* pocet cisel pre ktore bude inicializovana pamat */
+
+void print_numbers(int *numbers,int length);
+
+#endif // PMS_H_INCLUDED
